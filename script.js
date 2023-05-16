a73078e11458fcfa5299a102738503327ea3719e
jshiffert <jshiffert@oki.org>
2023-05-16 14:42:52 -0400

    added variables to store arrays for each character type

--- script.js
+++ script.js
@@ -56,7 +56,10 @@ function getPasswordOptions() {
    return passwordOptions;
 }
 
-
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".",",","/",":",";","?","[","]","{","}", "`", "~", "|", "<", ">", "=", "-", "_"];
 
 // Function for getting a random element from an array(all instances of arr will be replaced by an ACTUAL VALUE when we do our callback.)
 function getRandom(arr) {
